713921a710461f9b2a019803193dc3aad73e9eeb
kaneza-A 001 <kanezaa99@gmail.com>
2025-05-17 13:44:26 +0200

    Removed broken submodule

---
+++ src/Navbar.jsx
@@ -0,0 +1,17 @@
+function Home(){
+    return(
+        <div classname='top'>
+
+            <div id="logo"></div>
+            <div id="text">
+                <p id="p1"> Home</p>
+                <p id="p1"> Services</p>
+                <p id="p1"> About</p>
+                <p id="p1"> Contact</p>
+                <button id="register">Register</button>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
